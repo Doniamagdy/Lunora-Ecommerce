0b38d1fc19123829ff7a588f72231f347c53aa0b
Doniamagdy <donallino@gmail.com>
2025-10-24 22:56:42 +0300

    Initial Commit : Set up auth pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <BrowserRouter basename="/Lunora-Ecommerce"> 
+  <HashRouter> 
     <QueryClientProvider client={client}>
       <App />
     </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
